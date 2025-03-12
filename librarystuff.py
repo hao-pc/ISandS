371df3dfdd768142631ff9f685b0994890a1b0f5
hao-pc <75453061+hao-pc@users.noreply.github.com>
2025-03-12 11:32:26 +0700

    add requirements

--- librarystuff.py
+++
@@ -1,8 +0,0 @@
-import rupasportread as pr
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-
-s = pr.catching('passport2.jpg')
-
-print(s)
